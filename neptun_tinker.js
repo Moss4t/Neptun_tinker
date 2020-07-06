0e1433042bd1960bbaa3db4efb880d048c45be22
LR <lehorichard@gmail.com>
2020-07-06 10:23:36 +0200

    Add js file

---
+++ neptun_tinker.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name           Neptun Tinker
+// @namespace      http://example.org
+// @description    Neptun, de jobb!
+// @version        0.0.1
+// @include        https://*neptun*/*hallgato*/*
+// @include        https://*neptun*/*oktato*/*
+// @include        https://*hallgato*.*neptun*/*
+// @include        https://*oktato*.*neptun*/*
+// @include        https://netw*.nnet.sze.hu/hallgato/*
+// @include        https://nappw.dfad.duf.hu/hallgato/*
+// @include        https://host.sdakft.hu/*
+// @include        https://neptun.ejf.hu/ejfhw/*
+// @grant          GM.xmlHttpRequest
+// @grant          GM_xmlhttpRequest
+// @grant          GM.getValue
+// @grant          GM_getValue
+// @grant          GM.setValue
+// @grant          GM_setValue
+// @grant          GM.info
+// @grant          GM_info
+// @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+
+(function() {
+    "use strict";
+})();
