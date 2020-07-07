d491580e3311e05e0a3e8674650ac532fa24a22a
Bazsa47 <55391083+Bazsa47@users.noreply.github.com>
2020-07-07 12:19:21 +0200

    Color fixes
    Colors are now properly seen

--- neptun_tinker.js
+++ neptun_tinker.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Neptun Tinker
 // @namespace      http://example.org
-// @description    Neptun, de jobb!
-// @version        0.0.3
+// @description    Neptun, viszont a mi verziónk!
+// @version        0.0.5
 // @downloadURL    https://raw.githubusercontent.com/Moss4t/Neptun_tinker/master/neptun_tinker.js
 // @include        https://*neptun*/*hallgato*/*
 // @include        https://*neptun*/*oktato*/*
@@ -25,39 +25,98 @@
 // ==/UserScript==
 
 (function() {
-    "use strict";
+  "use strict";
 
-    var nep = {
-        init: async function() {
+  var nep = {
+    init: async function() {
 
-            var i;
-            var max_num = document.getElementById("c_messages_gridMessages_ddlPageSize").value;
-            for (i = 0; i < max_num; i++) {
+      this.initKeepSession();
+      this.newMenus();
+      this.hideHeader();
+      this.hideFilter();
 
-                const element = document.getElementsByClassName("link")[i];
+      var i;
+      var max_num = document.getElementById("c_messages_gridMessages_ddlPageSize").value;
+      for (i = 0; i < max_num; i++) {
 
-                if(element.innerHTML == "Kurzus órarendi változás" || element.innerHTML.toUpperCase().includes("EMLÉKEZTETŐ") || element.innerHTML.toUpperCase().includes("ERASMUS")
-                   || element.innerHTML.toUpperCase().includes("SPORT") || element.innerHTML.toUpperCase().includes("ANGOL") || element.innerHTML.toUpperCase().includes("ÉRTESÍTÉS")){
+        const element = document.getElementsByClassName("link")[i];
+
+          //NEM KÍVÁNATOS ÜZENETEK
+        if(element.innerHTML == "Kurzus órarendi változás" || element.innerHTML.toUpperCase().includes("EMLÉKEZTETŐ") || element.innerHTML.toUpperCase().includes("ERASMUS")
+        || element.innerHTML.toUpperCase().includes("SPORT") || element.innerHTML.toUpperCase().includes("ANGOL") || element.innerHTML.toUpperCase().includes("ÉRTESÍTÉS")){
                     const id = element.parentElement.parentElement.id;
                     document.getElementById(id).style.display = 'none';
                 }
+          //JEGYBEÍRÁS
                 else if (element.innerHTML.includes("jegybeírás történt") || element.innerHTML.includes("vizsgajegy került beírásra")){
                     const id = element.parentElement.parentElement.id;
                     document.getElementById(id).classList.remove("Row1_Bold");
-                    document.getElementById(id).style.backgroundColor = "#00FF00";
-                    console.log("Jegy!");
+                    if(document.getElementById(id).children[5].children[0].alt == "Elolvasott üzenet"){
+                        document.getElementById(id).style.backgroundColor = "#00FF00";
+                    }else{
+                        document.getElementById(id).style.backgroundColor = "#40D043";
+                        document.getElementById(id).style.color = "white";
+                        console.log(document.getElementById(id).children[6].children[0]);
+                       // document.getElementById(id).children[6].children[0].classList.remove("link");
+                    }
+
+          //VIZSGAKIÍRÁS
                 }
                 else if (element.innerHTML.includes("Új vizsgakiírás")){
                     const id = element.parentElement.parentElement.id;
-                    document.getElementById(id).classList.remove("Row1_Bold");
-                    document.getElementById(id).style.backgroundColor = "#1E90FF";
+                     document.getElementById(id).classList.remove("Row1_Bold");
+                     document.getElementById(id).style.color = "white";
+                     document.getElementById(id).children[6].children[0].style.setProperty("color", "white", "important");
+                    if(document.getElementById(id).children[5].children[0].alt == "Elolvasott üzenet"){
+                        document.getElementById(id).style.backgroundColor = "#6F9EFF";
+                    }else{
+                        document.getElementById(id).style.backgroundColor = "#3160F9";
+                        console.log(document.getElementById(id).children[6].children[0]);                       
+                    }
                 }
+          //ÖSZTÖNDÍJ
                 else if (element.innerHTML.toUpperCase().includes("ÖSZTÖNDÍJ")){
                     const id = element.parentElement.parentElement.id;
-                    document.getElementById(id).style.backgroundColor = "#FFD700";
+                    document.getElementById(id).classList.remove("Row1_Bold");
+                     if(document.getElementById(id).children[5].children[0].alt == "Elolvasott üzenet"){
+                        document.getElementById(id).style.backgroundColor = "#FFED6B";
+                    }else{
+                        document.getElementById(id).style.backgroundColor = "#F9DD31";
+                        console.log(document.getElementById(id).children[6].children[0]);
+                       //document.getElementById(id).children[6].children[0].classList.remove("link");
+                    }
+
                 }
             }
-            this.initKeepSession();
+
+            var table = document.getElementById("c_messages_gridMessages_bodytable");
+            var cell = table.getElementsByClassName("scrollablebody");
+            for (var j = 1, row; row = table.rows[j]; j++) {
+                let date = row.cells[7].textContent;
+                let str = date.split('.');
+                let str2 = str[3].split(':');
+                let newDate = new Date(str[0], str[1] - 1, str[2], str2[0], str2[1], str2[2]);
+                let now = Date.now();
+                let newText;
+                if (Math.floor((now - newDate) / (1000*60*60)) < 1) {
+                  newText = Math.floor(Math.floor(now - newDate) / (1000*60)) + " perce";
+                }
+                else if (Math.floor((now - newDate) / (1000*60*60*24)) < 1){
+                  newText = Math.floor(Math.floor(now - newDate) / (1000*60*60)) + " órája";
+                }
+                else if (Math.floor((now - newDate) / (1000*60*60*24)) < 365) {
+                  newText = Math.floor(Math.floor(now - newDate) / (1000*60*60*24)) + " napja";
+                } else {
+                  newText = Math.floor(Math.floor(now - newDate) / (1000*60*60*24*365)) + " éve";
+                }
+                if (newText != null) {
+                  row.cells[7].textContent = newText;
+                }
+              }
+          },
+
+        isPage: function(ctrl) {
+          return (window.location.href.indexOf("ctrl=" + ctrl) != -1);
         },
 
         initKeepSession: function() {
@@ -92,6 +151,74 @@
             }, 1000);
           },
 
+          hideHeader: function() {
+            $("#panHeader, #panCloseHeader").hide();
+            $("table.top_menu_wrapper").css("margin-top", "5px").css("margin-bottom", "8px");
+            $("#form1 > fieldset").css("border", "0 none");
+            $("#span_changeproject").parent().hide();
+          },
+
+
+          hideFilter: function() {
+            $("#function_tableheader, #upFilter").hide();
+            $("table.top_menu_wrapper").css("margin-top", "5px").css("margin-bottom", "8px");
+            $("#form1 > fieldset").css("border", "0 none");
+            $("#span_changeproject").parent().hide();
+          },
+
+          initParameters: function() {
+            nep.user = nep.getUser();
+            nep.domain = nep.getDomain();
+            nep.training = nep.getTraining();
+          },
+
+          newMenus: function(){
+            var orarend = $('<li aria-haspopup="false" tabindex="0" role="menuitem" class="menu-parent has-target" id="mb1_Orarend" targeturl="main.aspx?ctrl=0203&amp;ismenuclick=true">Órarend</li>');
+            $("#mb1_Targyak").before(orarend);
+            $("#mb1_Tanulmanyok_Órarend").remove();
+
+            if(!$("#upChooser_chooser_kollab").hasClass("KollabChooserSelected")) {
+              $('<li aria-haspopup="false" tabindex="0" role="menuitem" class="menu-parent has-target" id="mb1_MeetStreet" targeturl="javascript:__doPostBack(\'upChooser$btnKollab\',\'\')">Meet Street</li>').appendTo("#mb1");
+            }
+            if(!$("#upChooser_chooser_neptun").hasClass("NeptunChooserSelected")) {
+              $('<li aria-haspopup="false" tabindex="0" role="menuitem" class="menu-parent has-target" id="mb1_TanulmanyiRendszer" targeturl="javascript:__doPostBack(\'upChooser$btnNeptun\',\'\')">Neptun</li>').appendTo("#mb1");
+            }
+
+            $("#mb1 li[targeturl]").css("position", "relative").each(function() {
+              $(this).addClass("has-target");
+              var a = $('<a href="' + $(this).attr("targeturl") + '" style="display: block; position: absolute; left: 0; top: 0; width: 100%; height: 100%"></a>');
+              a.click(function(e) {
+                $("ul.menu").css("visibility", "hidden");
+                e.stopPropagation();
+              });
+              var hoverid = $(this).attr("hoverid");
+              if(hoverid) {
+                a.hover(function() { $(hoverid).addClass("menu-hover"); }, function() { $(hoverid).removeClass("menu-hover"); });
+              }
+              $(this).append(a);
+
+            });
+          },
+
+         /*moreMessages: function() {
+            window.setInterval(function() {
+              var pageSelect = $(".grid_pagerpanel select");
+              pageSelect.each(function() {
+                var e = $(this);
+                e.hide();
+                $(".link_pagesize", e.closest("tr")).html("");
+                if(e.attr("data-listing") != "1" && e.val() != "500") {
+                  e.attr("data-listing", "1").val("500");
+                  var onChange = this.getAttributeNode("onchange");
+                  if(onChange) {
+                    nep.runEval(onChange.value);
+                  }
+                }
+              });
+            }, 100);
+          },
+          */
+
           runEval: function(source) {
             if ("function" == typeof source) {
               source = "(" + source + ")();"
@@ -101,6 +228,10 @@
             script.textContent = source;
             document.body.appendChild(script);
             document.body.removeChild(script);
+          },
+
+          runAsync: function(func) {
+            window.setTimeout(func, 0);
           },
 
           getChild: function(o, s) {
@@ -113,7 +244,7 @@
             }
             return o;
           },
-    
+
           setChild: function(o, s, v) {
             while(s.length) {
               var n = s.shift();
@@ -132,6 +263,29 @@
               o = o[n];
             }
           },
+
+    fixOfficialMessagePopup: function() {
+        var dismiss = function() {
+          $("[aria-describedby=upRequiredMessageReader_upmodal_RequiredMessageReader_divpopup] .ui-dialog-content").dialog("close");
+        };
+
+        window.setInterval(function() {
+          var messagePopup = $("#upRequiredMessageReader_upmodal_RequiredMessageReader_divpopup:visible").closest(".ui-dialog");
+          if(messagePopup.size() > 0 && $("#upFunction_c_messages_upMain_upGrid").size() === 0) {
+            nep.runEval(dismiss);
+          }
+          if(messagePopup.size() > 0 && messagePopup.is(":not([data-nep-enhanced])")) {
+            messagePopup.attr("data-nep-enhanced", "true");
+            $("input[commandname=Tovabb]", messagePopup).val("Elolvasom");
+            var dismissBtn = $('<input value="Most nem érdekel" class="nep_dismiss ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" type="button">');
+            dismissBtn.click(function() {
+              nep.runEval(dismiss);
+            });
+            $(".ui-dialog-footerbar > div", messagePopup).append(dismissBtn);
+          }
+        }, 200);
+      },
+
     }
     nep.init();
   })();
