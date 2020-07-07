0a12c33dfdbb47b8892fef1f9ef62d1ecc7492aa
Lovász Ákos <akos4x44@gmail.com>
2020-07-07 10:15:17 +0200

    removed more messages function

--- neptun_tinker.js
+++ neptun_tinker.js
@@ -30,7 +30,7 @@
   var nep = {
     init: async function() {
       
-      this.moreMessages();
+      //this.moreMessages();
       
       var i;
       var max_num = document.getElementById("c_messages_gridMessages_ddlPageSize").value;
@@ -170,23 +170,23 @@
             });
           },
 
-          moreMessages: function() {
-            window.setInterval(function() {
-              var pageSelect = $(".grid_pagerpanel select");
-              pageSelect.each(function() {
-                var e = $(this);
-                e.hide();
-                $(".link_pagesize", e.closest("tr")).html("");
-                if(e.attr("data-listing") != "1" && e.val() != "500") {
-                  e.attr("data-listing", "1").val("500");
-                  var onChange = this.getAttributeNode("onchange");
-                  if(onChange) {
-                    nep.runEval(onChange.value);
-                  }
-                }
-              });
-            }, 100);
-          },
+          //moreMessages: function() {
+          //  window.setInterval(function() {
+          //    var pageSelect = $(".grid_pagerpanel select");
+          //    pageSelect.each(function() {
+          //      var e = $(this);
+          //      e.hide();
+          //      $(".link_pagesize", e.closest("tr")).html("");
+          //      if(e.attr("data-listing") != "1" && e.val() != "500") {
+          //        e.attr("data-listing", "1").val("500");
+          //        var onChange = this.getAttributeNode("onchange");
+          //        if(onChange) {
+          //          nep.runEval(onChange.value);
+          //        }
+          //      }
+          //    });
+          //  }, 100);
+          //},
 
           runEval: function(source) {
             if ("function" == typeof source) {
